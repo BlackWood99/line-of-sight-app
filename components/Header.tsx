b0e456ceca4fa74919817db6d6a98e5421a69ddf
BlackWood99 <78879606+BlackWood99@users.noreply.github.com>
2021-05-22 21:51:51 +0300

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,61 +1,45 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import Link from 'next/link'
+import React from "react"
+import styled from "@emotion/styled"
+import Link from "next/link"
+import Title from "./Title"
+import FlexBox from "./FlexBox"
 
 const Header = () => {
 
-    const Header = styled('header')`
-        padding: 2rem 0;
-        margin-bottom: 10px;
-    `
+	const Header = styled("header")`
+		padding: 2rem 0;
+		margin-bottom: 10px;
+	`
 
-    const Container = styled('div')`
-        display: flex;
-        justify-content: flex-end;
-
-        width: 100%;
-        max-width: 1460px;
-        margin: 0 auto;
-    `
-
-    const GameTitle = styled('h1')`
-        display: flex;
+	const Container = styled("div")`
+		display: flex;
+		justify-content: space-between;
         align-items: center;
-        text-align: center;
-        font-family: 'Roboto';
-        font-size: 54px;
-        font-weight: bold;
-        line-height: 65px;
-        color: #2B3172;
-    `
 
-    const Logo = styled('div')`
-        margin-left: 11%;
-    `
+		width: 100%;
+		max-width: 1460px;
+		margin: 0 auto;
+	`
 
-    const Img = styled('img')`
-        max-width: ${props => props.width || '200px'};
-        max-height: ${props => props.height || '90px'};
-        width: 100%;
-        height: 100%;
-    `
+	return (
+		<Header>
+			<Container>
+                <FlexBox width="10%"/>
 
-    return (
-        <Header>
-            <Container>
-                 
-                <GameTitle>Тренажер «Поле зрения»</GameTitle>
-            
-                <Logo>
-                    <Link href='/'>
-                        <a>
-                            <Img src='/logo.png' width={'260px'} height={'110px'}/>
-                        </a>
-                    </Link>
-                </Logo>
-            </Container>
-        </Header>
-    )
+				<Title color="#2B3172" fontSize={54} lineHeight={65}>
+					Тренажер «Поле зрения»
+				</Title>
+
+                <Link href='/'>
+                    <a>
+                        <Title color="#2B3172" fontSize={48} fontWeight={900}>
+                            LOGO
+                        </Title>
+                    </a>
+                </Link>
+			</Container>
+		</Header>
+	)
 }
 
 export default Header
